c1a0eed2dd2aae25772f26c49cc117b9092f815e
comerfpi <comerfpi@tcd.ie>
2024-06-13 16:11:00 -0700

    renamed so pmsim can be called as a macro in root

--- pmsim.C
+++ pmsim.C
@@ -16,10 +16,11 @@
 
 using namespace Garfield;
 
-int main() {
+int pmsim() {
 
   // Make LXe medium using NEST-generated gas table
   MediumMagboltz LXe;
   LXe.LoadGasFile("GasTable_Xe_165K");
 
 }
+main
