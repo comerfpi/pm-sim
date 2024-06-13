d2382da211cf621d3626ac0ea511c51d46ef6655
Pierce Comerford <comerfpi@tcd.ie>
2024-06-13 15:45:54 -0700

    Initial commit

---
+++ pm-sim.C
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+#include <TCanvas.h>
+#include <TROOT.h>
+#include <TApplication.h>
+
+#include "Garfield/ViewDrift.hh"
+
+#include "Garfield/ComponentAnalyticField.hh"
+#include "Garfield/MediumMagboltz.hh"
+#include "Garfield/Sensor.hh"
+#include "Garfield/DriftLineRKF.hh"
+#include "Garfield/TrackHeed.hh"
+
+using namespace Garfield;
+
+int main() {
+
+    MediumMagboltz }
