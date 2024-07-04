c9b9627d181acf1251066506f1e8f148a8f9fe1f
comerfpi <comerfpi@tcd.ie>
2024-07-04 13:23:55 -0700

    I want it to be square

--- pmsim.C
+++ pmsim.C
@@ -90,6 +90,10 @@ int main(int argc, char * argv[]) {
   ViewDrift driftView;
   driftline.EnablePlotting(&driftView);
 
+  ViewFEMesh viewMesh;
+  viewMesh.SetComponent(&fm);
+  viewMesh.SetCanvas(canvas);
+
    // //drift electrons using RKF method
   int totalElectron = 0;
   double avgTransparency=0;
@@ -158,6 +162,7 @@ int main(int argc, char * argv[]) {
   }
 outfile.close();
 
+  viewMesh.Plot();
   // Draw the drift view
   driftView.SetCanvas(canvas);
   driftView.Plot();
