508f72a9400818a81fc2c25cbf21c4668a3f456c
comerfpi <comerfpi@tcd.ie>
2024-07-17 15:21:23 -0700

    do i even need a detector?

--- driftvel.cpp
+++ driftvel.cpp
@@ -1,14 +1,10 @@
 #include <iostream>
 #include <NEST.hh>
-#include <DetectorExample_XENON10.hh>
 
 int main() {
     // Initialize NEST
     NEST::NESTcalc nestCalc;
     
-    // Set up the example detector
-    DetectorExample_XENON10 detector;
-
     double Ecg = 800; // Field between cathode and grid 1
     double Egg = 1600; // Field between two grids
     double Ega = 2400; // Field between grid 2 and anode
@@ -18,12 +14,6 @@ int main() {
     
     // Loop over each electric field value
     for (int i = 0; i < 3; ++i) {
-        // Set the electric field for the current iteration
-        detector.SetElectricField(electricFields[i]);
-        
-        // Get the density of liquid xenon at desired temperature and pressure
-        double density = detector.LiquidDensity();
-        
         // Calculate the drift velocity
         double driftVelocity = nestCalc.GetDriftVelocity(detector, density);
         
